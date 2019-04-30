82d2c6e07ede56efdf52113b98befd1ea210ad5b
Lucas Iankowski <lucasicsilva@hotmail.com>
2019-04-30 17:36:11 -0300

    Começamos a desenvolver o esqueleto do aplicativo

---
+++ Confort/Confort/GoalCell.swift
@@ -0,0 +1,18 @@
+//
+//  GoalCell.swift
+//  Confort
+//
+//  Created by Lucas Iankowski Corrêa da Silva on 30/04/19.
+//  Copyright © 2019 ADABestGroup. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell{
+    @IBOutlet weak var titleGoal: UILabel!
+    @IBOutlet weak var goalProgressBar: UIProgressView!
+    
+    
+}
+
+
