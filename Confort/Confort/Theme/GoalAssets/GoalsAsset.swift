e75de40a4f9a0a05751ffade1d6311924592b968
LuizGuerra <luizpedrofg@gmail.com>
2019-05-09 15:39:00 -0300

    Added some changes

---
+++ Confort/Confort/Theme/GoalAssets/GoalsAsset.swift
@@ -0,0 +1,15 @@
+//
+//  GoalsAssets.swift
+//  Confort
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 09/05/19.
+//  Copyright © 2019 ADABestGroup. All rights reserved.
+//
+
+import UIKit
+
+class GoalsAsset {
+    
+    static let checkButtonAsset = UIImage(named: "checkButtonAsset")
+    
+}
