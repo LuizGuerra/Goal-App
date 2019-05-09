e03633cbd03287ae2b395d8d148be3a90f84f093
LuizGuerra <luizpedrofg@gmail.com>
2019-05-09 17:28:18 -0300

    Started the page to add Goals

--- Confort/Confort/View/NewGoalFirstPage.swift
+++ Confort/Confort/View/NewGoalFirstPage.swift
@@ -10,28 +10,16 @@ import UIKit
 
 class NewGoalFirstPage: UIViewController{
     @IBOutlet weak var priorityBar: UISlider!
-//    @IBOutlet weak var goalLabel: UILabel!
     lazy var goalHolderLabel: UILabel? = view.viewWithTag(10) as? UILabel
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        if let aux = self.goalLabel {
-//            goalLabel.adjustsFontSizeToFitWidth = true
-//        }
-//        goalHolderLabel.adjustsFontSizeToFitWidth = true
-        print("View Did Load")
-//        print(view.subviews.last!.subviews.first!.subviews.first!)
         
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        print(goalHolderLabel)
     }
-//    override func viewWillAppear(_ animated: Bool) {
-//        priorityBar.setAppSliderDefaults()
-//    }
 }
 
